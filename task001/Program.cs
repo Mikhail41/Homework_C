0af7cdd76ab2d49c2cfddca1be92691aeba3aea1
Mikhail <mikhail-zmyslin@yandex.ru>
2022-06-26 21:28:44 +0300

    Решение первой задачи

---
+++ task001/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число :");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число :");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (b > a)
+{
+    Console.WriteLine("Максимальное число = ");
+    Console.WriteLine(b);
+    Console.WriteLine("Минимальное число = ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine("Максимальное число = ");
+    Console.WriteLine(a);
+    Console.WriteLine("Минимальное число = ");
+    Console.WriteLine(b);
+}
