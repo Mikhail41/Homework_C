31d3ee961f4b5542187047c655778207017e82f4
Mikhail <mikhail-zmyslin@yandex.ru>
2022-06-26 22:46:56 +0300

    Решение первой задачи,второй вариант

---
+++ task001_2/Program.cs
@@ -0,0 +1,20 @@
+﻿int a = -9;
+Console.WriteLine("Первое число = ");
+Console.WriteLine(a);
+int b = -3;
+Console.WriteLine("Второе число = ");
+Console.WriteLine(b);
+if (b > a)
+{
+    Console.WriteLine("Максимальное число = ");
+    Console.WriteLine(b);
+    Console.WriteLine("Минимальное число = ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine("Максимальное число = ");
+    Console.WriteLine(a);
+    Console.WriteLine("Минимальное число = ");
+    Console.WriteLine(b);
+}
