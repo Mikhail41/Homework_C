d2a21ea320c8b9e6dcd510ac2f7bb3e081962e95
Mikhail <mikhail-zmyslin@yandex.ru>
2022-06-26 22:59:13 +0300

    Решение второй задачи,второй вариант

---
+++ task002_2/Program.cs
@@ -0,0 +1,25 @@
+﻿int num1 = 44;
+Console.WriteLine("Первое число = ");
+Console.WriteLine(num1);
+int num2 = 5;
+Console.WriteLine("Второе число = ");
+Console.WriteLine(num2);
+int num3 = 78;
+Console.WriteLine("Третье число = ");
+Console.WriteLine(num2);
+
+int max = (0);
+if (num1 > max)
+
+    max = num1;
+
+if (num2 > max)
+
+    max = num2;
+
+if (num3 > max)
+
+    max = num3;
+
+System.Console.WriteLine("Максимальное число: ");
+System.Console.WriteLine(max);
